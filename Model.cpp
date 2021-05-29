4e683bd2818699538a57ba341f86bf52075b7b4b
Aaron Willisford <aaronwillisford@gmail.com>
2021-05-29 01:33:23 -0400

    working on weights vector still, problem with adding float values to final vector

--- Model.cpp
+++ Model.cpp
@@ -5,21 +5,37 @@ Model::Model(std::vector<float> input, uint numLayers, uint neuronsPerLayer, flo
     // assign class attribute to constructor parameter
     this->learningRate = learningRate;
 
-    // create vector for first layer's weights
+    // create vector for input layer's weights
     weights.push_back(new std::vector<std::vector<float>*>);
     // create a weight vector that holds type float for each node in input
-    for(int it = 0; it < input.size() * 2; ++it) { // 2 inputs
+    for (int it = 0; it < input.size() * 2; ++it) { // 2 inputs
         weights[0]->push_back(new std::vector<float>);
     }
 
-    // create layers
-    for(uint x = 0; x < numLayers; ++x) {
+    // create layers ----- UPDATE COMMENT
+    for (uint x = 0; x < numLayers; ++x) {
         layers.push_back(new std::vector<uint>);
-        // create neurons
+        weights.push_back(new std::vector<std::vector<float>*>); // create hidden layer weights
+        // create neurons  ------- UPDATE COMMENT
         for (uint z = 0; z < neuronsPerLayer; ++z) {
-            /* I am using the arrow operator in this situation
-             * since each element in layers is a vector pointer */
-            layers[x]->push_back(z); 
+            layers[x]->push_back(z);
+            weights[x + 1]->push_back(new std::vector<float>); // creates weight vector for each node in hidden layers
+        }
+    }
+
+    for (uint x = 0; x < numLayers + 1; ++x) {
+        for (uint z = 0; z < neuronsPerLayer; ++z) {
+
+            // FIGURE OUT WHY THIS WORKS
+            for(int y = 0; y < neuronsPerLayer; ++y) {
+                float temp = 1.0;
+                weights[x][z][y]->push_back(temp);
+            }
+
+            // BUT THIS DOESNT?!?!?
+            float temp = 1.0;
+            weights[x][z].push_back(temp);
+
         }
     }
 }
