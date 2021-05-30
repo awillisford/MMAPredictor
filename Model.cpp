0d834116416eb50b8f1057203e29b66edb52428a
Aaron Willisford <aaronwillisford@gmail.com>
2021-05-30 06:44:34 -0400

    finalized weight vector creation

--- Model.cpp
+++ Model.cpp
@@ -23,10 +23,22 @@ Model::Model(std::vector<float> input, uint numLayers, uint neuronsPerLayer, flo
         }
     }
 
+    // assign weights for each node past input layer
     for (uint x = 0; x < numLayers + 1; ++x) {
+        float temp = 1.0; // temporary float variable to push back into vector
         for (uint z = 0; z < neuronsPerLayer; ++z) {
-            float temp = 1.0;
-            (*weights[x])[z]->push_back(temp);
+            // assign weights per node based on number of neurons in each layer
+            if (x < numLayers) {
+                for (int it = 0; it < neuronsPerLayer; ++it) {
+                    (*weights[x])[z]->push_back(temp);
+                }
+            }
+            // create two weights per node in last hidden layer since there are two outputs
+            else {
+                for (int it = 0; it < 2; ++it) {
+                    (*weights[x])[z]->push_back(temp);
+                }
+            }
         }
     }
 }
