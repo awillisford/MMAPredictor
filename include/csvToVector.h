be7105742af3253a2440e85087675b3e8c4bbe30
Aaron Willisford <aaronwillisford@gmail.com>
2021-06-08 06:39:15 -0400

    added temporary comments

--- include/csvToVector.h
+++ include/csvToVector.h
@@ -3,14 +3,18 @@
 #include <vector>
 #include <string>
 
-class Data {
+class csvToVector {
     private:
+    // TEMP COMMENT -- UPDATE
     static bool contains_number(const std::string& str);
 
+    // TEMP COMMENT -- UPDATE 
     static float contains_text(const std::string& str);
 
+    // TEMP COMMENT -- UPDATE
     static float check_cell_type(std::string& cell);
 
     public:
+    // TEMP COMMENT -- UPDATE
     static std::vector<std::vector<float>> extract_data();
 };
