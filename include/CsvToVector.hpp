9b59924f99ccab2228c336e54d3469cf3348b801
Aaron Willisford <aaronwillisford@gmail.com>
2021-06-11 04:42:16 -0400

    updated comments

--- include/CsvToVector.hpp
+++ include/CsvToVector.hpp
@@ -5,21 +5,22 @@
 
 class CsvToVector {
     private:
-    // TEMP COMMENT -- UPDATE
+    // determine if cell contains number values or not
     static bool contains_number(const std::string& str);
 
-    // assign float values to each of the different text
+    // determine if cell contains text or not, and assigns value to text
     static float contains_text(const std::string& str);
 
-    // TEMP COMMENT -- UPDATE
+    // sends cell values through contains_number() or contains_text(), and returns new float value
     static float check_cell_type(const std::string& cell);
 
-    // TEMP COMMENT -- UPDATE
+    // used for comparing difference between different float values, and placement in either features or labels
     static bool compare_floats(float& a, float b, float epsilon = 0.01f);
 
     public:
-    // TEMP COMMENT -- UPDATE
+    // takes data from csv and puts data into matrix of float values
     static void extract_data(const std::string& csvFile);
+
 
     // class members
     static std::vector<std::vector<float>> features;
