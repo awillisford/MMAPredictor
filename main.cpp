546f087373d3c24b46c88b5261b251dd25e881bb
Aaron Willisford <aaronwillisford@gmail.com>
2021-07-23 00:41:14 -0400

    achieved 59.4225% on random model test

--- main.cpp
+++ main.cpp
@@ -4,22 +4,32 @@
 #include "include/Model.hpp"
 
 int main() {
-    std::vector<std::vector<std::vector<float>>> data = CsvToVector::extract_data("data/data_filtered.csv");
+    std::vector<std::vector<std::vector<float>>> data = CsvToVector::extract_data("data/test.csv");
     std::vector<std::vector<float>> features = data[0];
     std::vector<std::vector<float>> labels = data[1];
 
-    Model m1(features, 2, 200, 0.1);
+    Model m1(features, 1, 200, 0.01);
 
     m1.randomize(); // randomize weights and biases
 
-    int EPOCHS = 5;
+    for (auto& x : features[0]) {
+        std::cout << x << " ";
+    }
+    std::cout << "\n\n";
+
+    int EPOCHS = 1;
 
     for (int epoch = 0; epoch < EPOCHS; ++epoch) {
         for (int elem = 0; elem < features.size(); ++elem) {
+            // std::cout << m1 << "---------------------------------------------------------------------------------------\n";
             m1.forward(features[elem]);
+            // std::cout << m1 << "---------------------------------------------------------------------------------------\n";
             m1.backward(features[elem], labels[elem]);
+            // std::cout << m1 << '\n';
+            // return 1;
         }
         std::cout << "epoch(" << epoch << ") : ";
         m1.printLoss(features);
     }
+    // std::cout << m1;
 } 
