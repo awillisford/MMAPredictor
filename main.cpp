6fdd9c043780d02674bb7d7884d38608b87ca324
Aaron Willisford <aaronwillisford@gmail.com>
2021-07-24 01:42:11 -0400

    created difference between testing and training data

--- main.cpp
+++ main.cpp
@@ -8,28 +8,33 @@ int main() {
     std::vector<std::vector<float>> features = data[0];
     std::vector<std::vector<float>> labels = data[1];
 
-    Model m1(features, 1, 200, 0.01);
+    std::vector<std::vector<float>> train_features(&features[0], &features[2600]);
+    std::vector<std::vector<float>> test_features(&features[2600], &features.back()+1);
 
-    m1.randomize(); // randomize weights and biases
+    std::vector<std::vector<float>> train_labels(&labels[0], &labels[2600]);
+    std::vector<std::vector<float>> test_labels(&labels[2600], &labels.back()+1);
 
-    for (auto& x : features[0]) {
-        std::cout << x << " ";
-    }
-    std::cout << "\n\n";
+    Model m1(features, 1, 200, 0.005);
 
-    int EPOCHS = 1;
+    m1.randomize(4); // randomize weights and biases
 
+    int EPOCHS = 5;
+
+    // train
+    std::cout << "Training:\n";
     for (int epoch = 0; epoch < EPOCHS; ++epoch) {
-        for (int elem = 0; elem < features.size(); ++elem) {
-            // std::cout << m1 << "---------------------------------------------------------------------------------------\n";
-            m1.forward(features[elem]);
-            // std::cout << m1 << "---------------------------------------------------------------------------------------\n";
-            m1.backward(features[elem], labels[elem]);
-            // std::cout << m1 << '\n';
-            // return 1;
+        for (int elem = 0; elem < train_features.size(); ++elem) {
+            m1.forward(train_features[elem], train_labels[elem]);
+            m1.backward(train_features[elem], train_labels[elem]);
         }
         std::cout << "epoch(" << epoch << ") : ";
-        m1.printLoss(features);
+        m1.printLoss(train_features);
     }
-    // std::cout << m1;
+
+    // test
+    std::cout << "\nTesting:\n";
+    for (int elem = 0; elem < test_features.size(); ++elem) {
+        m1.forward(test_features[elem], test_labels[elem]);
+    }
+    m1.printLoss(test_features);
 } 
